fe265f76dcce091e853863fc75e0859889ce7842
aaa <aaa@lkds.ru>
2022-07-18 19:21:09 +0700

    need add web view for desktop and web

---
+++ lib/flutter_dev_page.dart
@@ -0,0 +1,262 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/rendering.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart';
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class FlutterDevPage extends StatefulWidget {
+  const FlutterDevPage({Key? key}) : super(key: key);
+
+  @override
+  State<FlutterDevPage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<FlutterDevPage> {
+  final TextEditingController _textEditingController = TextEditingController();
+  late WebViewController _controllerWebView;
+
+  bool get isMobile => platform == Platform.isIOS || Platform.isAndroid;
+
+  String enterText = '';
+  String headerText = '';
+  String corsText = '';
+  String descriptionText = '';
+  String platform = '';
+
+  @override
+  void initState() {
+    super.initState();
+    kIsWeb ? platform = 'web' : platform = Platform.operatingSystem;
+  }
+
+  void _loadHtmlPage(String uri) async {
+    final result = await http.get(Uri.parse(uri));
+    var cors = 'none';
+    result.headers.forEach((key, value) {
+      if (key == 'access-control-allow-origin') {
+        cors = value;
+      }
+      if (kDebugMode) {
+        print('key $key = value $value');
+      }
+    });
+
+    var document = parse(result.body);
+    var title = document.getElementsByTagName('title')[0].text;
+
+    if (kDebugMode) {
+      print("title = $title");
+    }
+    setState(() {
+      headerText = title;
+      corsText = 'CORS Header: $cors';
+      descriptionText = result.body;
+      _controllerWebView.loadUrl(enterText);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // resizeToAvoidBottomInset: false,
+      // body: SingleChildScrollView(
+      //   physics: const NeverScrollableScrollPhysics(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          // mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            headerText == ''
+                ? const SizedBox(
+                    height: 0,
+                  )
+                : Text(
+                    headerText,
+                    style: const TextStyle(
+                        fontSize: 30,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+            corsText == ''
+                ? const SizedBox(
+                    height: 0,
+                  )
+                : Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      corsText,
+                      style: const TextStyle(fontSize: 25, color: Colors.red),
+                    ),
+                  ),
+            Flexible(
+                child: isMobile
+                    ? WebView(
+                        initialUrl: enterText,
+                        onWebViewCreated:
+                            (WebViewController webViewController) {
+                          _controllerWebView = webViewController;
+                        },
+                      )
+                    : const SizedBox(height: 0,)
+                    //TODO desktop and web WebView
+                // canLaunchUrlString(enterText)
+            ),
+            headerText == ''
+                ? const SizedBox(
+                    height: 0,
+                  )
+                : const Divider(
+                    color: Colors.black,
+                    height: 20,
+                    thickness: 3,
+                  ),
+            IntrinsicHeight(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: TextField(
+                        controller: _textEditingController,
+                        onChanged: (text) {
+                          enterText = text;
+                        },
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'URL',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: ElevatedButton(
+                      onPressed: () => _loadHtmlPage(enterText),
+                      child: const Text(
+                        'LOAD',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Text('APPLICATION RUNNING ON $platform'.toUpperCase()),
+          ],
+        ),
+      ),
+      // ),
+    );
+  }
+
+// @override
+// Widget build(BuildContext context) {
+//   return Scaffold(
+//     // resizeToAvoidBottomInset: false,
+//     // body: SingleChildScrollView(
+//     //   physics: const NeverScrollableScrollPhysics(),
+//     body: Padding(
+//       padding: const EdgeInsets.all(20.0),
+//       child: Column(
+//         // mainAxisSize: MainAxisSize.min,
+//         children: <Widget>[
+//           headerText == ''
+//               ? const SizedBox(
+//                   height: 0,
+//                 )
+//               : Text(
+//                   headerText,
+//                   style: const TextStyle(
+//                       fontSize: 30,
+//                       color: Colors.black,
+//                       fontWeight: FontWeight.bold),
+//                 ),
+//           corsText == ''
+//               ? const SizedBox(
+//                   height: 0,
+//                 )
+//               : Align(
+//                   alignment: Alignment.centerLeft,
+//                   child: Text(
+//                     corsText,
+//                     style: const TextStyle(fontSize: 25, color: Colors.red),
+//                   ),
+//                 ),
+//           Flexible(
+//             child: SingleChildScrollView(
+//               child: Text(descriptionText),
+//             ),
+//           ),
+//           headerText == ''
+//               ? const SizedBox(
+//                   height: 0,
+//                 )
+//               : const Divider(
+//                   color: Colors.black,
+//                   height: 20,
+//                   thickness: 3,
+//                 ),
+//           IntrinsicHeight(
+//             child: Row(
+//               crossAxisAlignment: CrossAxisAlignment.stretch,
+//               children: [
+//                 Expanded(
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(5.0),
+//                     child: TextField(
+//                       controller: _textEditingController,
+//                       onChanged: (text) {
+//                         enterText = text;
+//                       },
+//                       obscureText: true,
+//                       decoration: const InputDecoration(
+//                         border: OutlineInputBorder(),
+//                         labelText: 'URL',
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//                 Padding(
+//                   padding: const EdgeInsets.all(5.0),
+//                   child: ElevatedButton(
+//                     onPressed: () => _loadHtmlPage(enterText),
+//                     child: const Text(
+//                       'LOAD',
+//                       style: TextStyle(fontSize: 20),
+//                     ),
+//                   ),
+//                 )
+//               ],
+//             ),
+//           ),
+//           Text('APPLICATION RUNNING ON $platform'.toUpperCase()),
+//         ],
+//       ),
+//     ),
+//     // ),
+//   );
+// }
+}
+
+// class HyperLink extends StatelessWidget {
+//   final String link;
+//   const HyperLink({Key? key, required this.link}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return GestureDetector(child: Center(
+//       child: Text(link),
+//     ),
+//     onTap: canLaunchUrlString(link),);
+//   }
+// }
+//
